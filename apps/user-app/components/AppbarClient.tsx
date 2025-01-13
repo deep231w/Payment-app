83149a7c7ec196e84db69075b0cd6acf7d5e3ca2
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-01-13 19:13:32 +0530

    error fixed

--- apps/user-app/components/AppbarClient.tsx
+++ apps/user-app/components/AppbarClient.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { signIn, signOut, useSession } from "next-auth/react";
 //import { Appbar } from "@repo/ui/appbar";
-import {Appbar} from "@repo/ui/appbar"
+import {Appbar} from ".././../../packages/ui/src/Appbar"
 import { useRouter } from "next/navigation";
 
 export function AppbarClient() {
